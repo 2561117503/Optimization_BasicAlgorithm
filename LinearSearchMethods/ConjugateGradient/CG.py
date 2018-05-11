1207233f6572227af2440a97ae68a05f9f36064d
YEN-GitHub <1274491995@qq.com>
2018-05-11 22:59:09 +0800

    add Conjugate Gradient

--- LinearSearchMethods/ConjugateGradient/CG.py
+++ LinearSearchMethods/ConjugateGradient/CG.py
@@ -1,12 +1,11 @@
 #coding=utf-8
 # @Author: yangenneng
-# @Time: 2018-05-11 20:56
-# @Abstract：Conjugate Gradient Preliminary Version
-
+# @Time: 2018-05-11 22:56
+# @Abstract：Conjugate Gradient  Version
 import numpy as np
 from numpy import *
 
-def CG_Preliminary(A,b,x_k):
+def CG(A,b,x_k):
     k = 0
     r_k  = A * x_k  - b  # r0
     p_k = -r_k           # p0
@@ -17,10 +16,11 @@ def CG_Preliminary(A,b,x_k):
     while r_k[0] >= 1e-8:
         r_k.shape=(2,1)
         p_k.shape=(2,1)
-        alpha_k = - (np.transpose(r_k) * p_k) / (np.transpose(p_k) * A * p_k)
+        alpha_k =  (np.transpose(r_k) * r_k) / (np.transpose(p_k) * A * p_k)
         x_k = x_k +  p_k * alpha_k
-        r_k = A * x_k - b
-        beta_k = (np.transpose(r_k) * A * p_k) / (np.transpose(p_k) * A * p_k)
+        r_k_old = r_k
+        r_k = A * p_k * alpha_k + r_k
+        beta_k = (np.transpose(r_k) * r_k ) / (np.transpose(r_k_old) * r_k_old)
         p_k = - r_k +   p_k * beta_k
 
         xarrar.append(x_k)
@@ -31,16 +31,19 @@ def CG_Preliminary(A,b,x_k):
     print("\nrkarrar",str(rkarrar))
     return x_k
 
+
+
 if __name__ == "__main__":
     A= mat([[4.0, 1.0], [1.0, 3.0]])
     b = mat([[1.0], [2.0]])
     x_0 = mat([[2.0], [1.0]])
-    xStar = CG_Preliminary(A, b, x_0)
+    xStar = CG(A, b, x_0)
     print("\n x_star:",str(xStar))
 
 
 '''
 result:
+F:\python-3.5\Anaconda3-4.2.0-Windows-x86_64\python.exe D:/Python/PyCharm-WorkSpace/Optimization_BasicAlgorithm/LinearSearchMethods/ConjugateGradient/CG.py
 xarrar [matrix([[2.],
         [1.]]), matrix([[0.23564955],
         [0.33836858]]), matrix([[0.09090909],
@@ -48,10 +51,12 @@ xarrar [matrix([[2.],
 
 rkarrar [matrix([[8.],
         [3.]]), matrix([[ 0.28096677],
-        [-0.74924471]]), matrix([[4.4408921e-16],
-        [0.0000000e+00]])]
+        [-0.74924471]]), matrix([[-5.55111512e-17],
+        [ 0.00000000e+00]])]
 
  x_star: [[0.09090909]
  [0.63636364]]
+
+Process finished with exit code 0
 
 '''
